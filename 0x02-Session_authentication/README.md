ad5fbaadb0e6be864c4886b0933b2dcb0bbb8ce3
Badrdineyk <badrdineyk9@gmail.com>
2024-11-14 07:51:18 -0800

    Tasks are solved

--- 0x02-Session_authentication/README.md
+++ 0x02-Session_authentication/README.md
@@ -1,1 +1,4 @@
-# 0x02. Session authentication
+# Session Authentication
+
+This project contains tasks for learning to authenticate a user through session authentication.
+
--- 0x02-Session_authentication/api/v1/auth/basic_auth.py
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module for basic authentication in the API.
+"""Basic authentication module for the API.
 """
 import re
 import base64
@@ -11,13 +11,15 @@ from models.user import User
 
 
 class BasicAuth(Auth):
-    """Class for handling basic authentication.
+    """Basic authentication class.
     """
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
-        """Extracts the Base64-encoded token"""
-        if isinstance(authorization_header, str):
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
             pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
@@ -26,9 +28,11 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(
             self,
-            base64_authorization_header: str) -> str:
-        """Decodes a Base64-encoded authorization."""
-        if isinstance(base64_authorization_header, str):
+            base64_authorization_header: str,
+            ) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
             try:
                 res = base64.b64decode(
                     base64_authorization_header,
@@ -40,9 +44,12 @@ class BasicAuth(Auth):
 
     def extract_user_credentials(
             self,
-            decoded_base64_authorization_header: str) -> Tuple[str, str]:
-        """Extracts user credentials from"""
-        if isinstance(decoded_base64_authorization_header, str):
+            decoded_base64_authorization_header: str,
+            ) -> Tuple[str, str]:
+        """Extracts user credentials from a base64-decoded authorization
+        header that uses the Basic authentication flow.
+        """
+        if type(decoded_base64_authorization_header) == str:
             pattern = r'(?P<user>[^:]+):(?P<password>.+)'
             field_match = re.fullmatch(
                 pattern,
@@ -58,6 +65,24 @@ class BasicAuth(Auth):
             self,
             user_email: str,
             user_pwd: str) -> TypeVar('User'):
-        """Retrieves a User object based"""
-        if isinstance(user_email, str) and isinstance(user_pwd, str):
-            pass
+        """Retrieves a user based on the user's authentication credentials.
+        """
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
